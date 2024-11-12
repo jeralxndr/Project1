d9e83c6c1432d760bdac8ff9a23a2c3616368174
jeralxndr <jeremy.alexander4@ontariotechu.net>
2024-11-11 20:36:51 -0500

    changed colour, finished delete function
    changed highlight colour for task and delete function completed

--- script.js
+++ script.js
@@ -30,7 +30,19 @@ var app = new function(){
     };
 
     this.Edit = function(item){
+        el = document.getElementById('edit-todo');
+        el.value = this.tasks[item]
+        document.getElementById('edit-box').style.display = 'block';
+        self=this;
 
+        document.getElementById('save-edit').onsubmit = function(){
+            var task = el.value;
+            if(task){
+                self.tasks.splice(item, 1, task.trim());
+                self.FetchAll();
+                CloseInput();
+            }
+        }
     };
 
     this.Delete = function(item){
